04ffd8dc1d6689e402714756b8fb6e7c826d3de6
Sam-d23 <samkiragu05@gmail.com>
2023-10-30 15:15:35 -0700

    Task 13.new

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 98  # LOAD_CONST 1 (98)
-    result += a  # LOAD_FAST 0 (a)
-    result = result ** b  # BINARY_POWER
-    result += b  # LOAD_FAST 1 (b)
-    return result  # RETURN_VALUE
-
+result = 98
+result += a ** b
+return result
