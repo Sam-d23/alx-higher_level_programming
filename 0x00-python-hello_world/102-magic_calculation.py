28e07140dce631284f14d8f230678433bd05353b
Sam-d23 <samkiragu05@gmail.com>
2023-10-30 15:08:47 -0700

    Correction

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-return (98 + (a ** b))
+    result = 98  # LOAD_CONST 1 (98)
+    result += a  # LOAD_FAST 0 (a)
+    result = result ** b  # BINARY_POWER
+    result += b  # LOAD_FAST 1 (b)
+    return result  # RETURN_VALUE
 
