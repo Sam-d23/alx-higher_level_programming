6345d6c8d6de4088beb051d97269b698bd72777d
Sam-d23 <samkiragu05@gmail.com>
2023-11-02 17:07:51 -0700

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    x = len(argv) - 1
-    if x == 0:
-        print("{:d}".format(x))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= x:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
-
 if __name__ == "__main__":
     import sys
-    add_arg(sys.argv)
+    x = 0
+    for j in range(len(sys.argv) - 1):
+        x += int (sys.argv[j + 1])
+        print("{}".format(total))
