3c0430863d8120e7a9a8bb6b77841a8df3c8822c
Sam-d23 <samkiragu05@gmail.com>
2023-11-06 15:29:04 -0800

    TASK 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,75 +1,99 @@
-#include "lists.h"
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
- *
- * Return: pointer to the first node in the new list
- */
-void reverse_listint(listint_t **head)
-{
-  listint_t *prev = NULL;
-  listint_t *current = *head;
-  listint_t *next = NULL;
+#include <stddef.h>
+#include "lists.h"
 
-  while (current)
-    {
-      next = current->next;
-      current->next = prev;
-      prev = current;
-      current = next;
+/* Define a singly linked list structure */
+struct listint_t {
+    int data;
+    struct listint_t *next;
+};
+
+typedef struct listint_t listint_t;
+
+/* Helper function to reverse a linked list */
+listint_t *reverseList(listint_t *head) {
+    listint_t *prev = NULL;
+    listint_t *current = head;
+    listint_t *next = NULL;
+
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
     }
 
-  *head = prev;
+    return prev;
 }
 
-/**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
- *
- * Return: 1 if it is, 0 if not
- */
-int is_palindrome(listint_t **head)
-{
-  listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
-
-  if (*head == NULL || (*head)->next == NULL)
-    return (1);
-
-  while (1)
-    {
-      fast = fast->next->next;
-      if (!fast)
-	{
-	  dup = slow->next;
-	  break;
-	}
-      if (!fast->next)
-	{
-	  dup = slow->next->next;
-	  break;
-	}
-      slow = slow->next;
+/* Function to check if a linked list is a palindrome */
+int is_palindrome(listint_t **head) {
+    if (*head == NULL || (*head)->next == NULL) {
+        return 1;  // An empty list or a single-element list is a palindrome.
     }
 
-  reverse_listint(&dup);
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev_slow = *head;
+    listint_t *second_half = NULL;
+    int is_palindrome = 1;
 
-  while (dup && temp)
-    {
-      if (temp->n == dup->n)
-	{
-	  dup = dup->next;
-	  temp = temp->next;
-	}
-      else
-	return (0);
+    while (fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
+        prev_slow = slow;
+        slow = slow->next;
     }
 
-  if (!dup)
-    return (1);
+    if (fast != NULL) {
+        // List has an odd number of elements; skip the middle element.
+        slow = slow->next;
+    }
 
-  return (0);
+    second_half = reverseList(slow);
+    slow = *head;
+
+    while (second_half != NULL) {
+        if (second_half->data != slow->data) {
+            is_palindrome = 0;
+            break;
+        }
+        second_half = second_half->next;
+        slow = slow->next;
+    }
+
+    // Restore the original list by reversing the second half again.
+    reverseList(second_half);
+
+    return is_palindrome;
 }
 
+// Function to insert a node at the beginning of the list for testing
+void insertAtBeginning(listint_t **head, int data) {
+    listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+    if (new_node == NULL) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(1);
+    }
+    new_node->data = data;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+int main() {
+    listint_t *head = NULL;
+    int values[] = {1, 2, 3, 2, 1};
+
+    for (int i = 0; i < 5; i++) {
+        insertAtBeginning(&head, values[i]);
+    }
+
+    int result = is_palindrome(&head);
+    if (result) {
+        printf("The list is a palindrome.\n");
+    } else {
+        printf("The list is not a palindrome.\n");
+    }
+
+    return 0;
+}
