2de9ba941328370c7d3306b9d78d5c904ebdaba6
Sam-d23 <samkiragu05@gmail.com>
2023-11-20 13:03:25 -0800

    Task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as e:
-        print("Exception:", e, file=sys.stderr)
+    except (ValueError, TypeError):
+        error_message = "Exception: Cannot print non-integer value"
+        print(error_message, file=sys.stderr)
         return False
