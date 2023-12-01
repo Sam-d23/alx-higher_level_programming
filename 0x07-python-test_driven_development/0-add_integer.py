65c58c74b3ea4f7317de9bb64c226b5e66538ce3
Sam-d23 <samkiragu05@gmail.com>
2023-12-01 05:52:53 -0800

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,9 +3,7 @@
 
 
 def add_integer(a, b=98):
-    """Returns reult of integer addition"""
-    if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    return int(a) + int(b)
