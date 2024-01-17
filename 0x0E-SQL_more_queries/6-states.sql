c33f0f22a5bfece28ac6f3515fbdc80cea27c676
Sam-d23 <samkiragu05@gmail.com>
2024-01-17 12:37:14 -0800

    06

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,5 @@
 --(in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-       id INT AUTO_INCREMENT,
-       name VARCHAR(256) NOT NULL,
-       PRIMARY KEY (id)
-);
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(256) NOT NULL);
