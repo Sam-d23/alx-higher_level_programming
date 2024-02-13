7ecda2177856df573b607be5b8cf8da54507f6c7
Sam-d23 <samkiragu05@gmail.com>
2024-02-13 11:36:33 -0800

    3.new

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,23 +1,20 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-            return {};
-        }
-        this.width = w;
-        this.height = h;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print() {
-        if (Object.keys(this).length === 0) {
-            return;
-        }
-        const row = 'X'.repeat(this.width);
-        for (let i = 0; i < this.height; i++) {
-            console.log(row);
-        }
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      const template = [];
+      for (let j = 0; j < this.width; j++) template.push('X');
+      console.log(`${template.join('')}`);
     }
+  }
 }
 
 module.exports = Rectangle;
