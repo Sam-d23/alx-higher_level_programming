ddab84e172396e5b75e7e32ccb374d74b7484714
Sam-d23 <samkiragu05@gmail.com>
2024-02-13 11:43:00 -0800

    4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      const temp = [];
+      for (let y = 0; y < this.width; y++) temp.push('X');
+      console.log(`${temp.join('')}`);
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+}
+
+module.exports = Rectangle;
