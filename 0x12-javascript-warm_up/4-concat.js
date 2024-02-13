33f922b86a9ffe04af341440dcc878d975a50170
Sam-d23 <samkiragu05@gmail.com>
2024-02-12 22:18:54 -0800

    4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv[2] !== undefined && process.argv[3] !== undefined)
+{
+console.log(process.argv[2] + ' is ' + process.argv[3]);
+} 
+else
+{
+console.log("Error: Please enter two arguments");
+}
+
