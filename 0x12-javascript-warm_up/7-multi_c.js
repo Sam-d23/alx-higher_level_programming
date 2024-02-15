d819caa356da512d45ba76780a47ca578b107496
Sam-d23 <samkiragu05@gmail.com>
2024-02-14 16:31:55 -0800

    07

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
-const { argv } = require('process');
-const x = parseInt(argv[2]);
-const msg = (cout) => {
-  for (; cout > 0; cout--) console.log('C is fun');
-};
-
-Number.isInteger(x) ? cout(x) : console.log('Missing number of occurrences');
+const argv = process.argv;
+const num = parseInt(argv[2]);
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
