fa44d017cddbd8144264ac441895abaf8998c2a9
Sam-d23 <samkiragu05@gmail.com>
2024-02-17 09:48:42 -0800

    0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,38 +1,25 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
+"""All states from the database hbtn_0e_0_usa are listed"""
 
-if __name__ == "__main__":
-    # Checks if correct number of arguments provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb as mysql
 
-    # Retrieves MySQL credentials from command line arguments
-    username, password, database = sys.argv[1:]
+    try:
+        db = mysql.connect(host='localhost', port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    except Exception:
+        print('Failed connecting to the database')
+        exit(0)
 
-    # Connects to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    # Creates a cursor object to execute queries
     cursor = db.cursor()
 
-    # Executes query to fetch all states sorted by id
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
 
-    # Fetches all the rows
-    states = cursor.fetchall()
+    result = cursor.fetchall()
 
-    # Displays the results
-    for state in states:
-        print(state)
+    for row in result:
+        print(row)
 
-    # Closes the cursor and database connection
     cursor.close()
     db.close()
