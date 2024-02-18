7e55c693730f1373e86ef2934f792bfa4529d561
Sam-d23 <samkiragu05@gmail.com>
2024-02-18 02:11:31 -0800

    6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" connect to a MySQL server running on localhost at port 3306"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
