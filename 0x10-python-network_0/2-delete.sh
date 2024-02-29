d6403bcc04fc6b4fafe407cb8de4a8becc134d9a
Sam-d23 <samkiragu05@gmail.com>
2024-02-29 12:33:02 -0800

    2

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Sends a DELETE request to the URL passed as the
-#first argument and displays the body of the response
-curl -X DELETE -s "$1"
+#Sends a DELETE request to the URL passed
+curl -sX DELETE "$1"
