1cfa3ad2cc789a0220816dc1cb21d47d61d9ab42
Sam-d23 <samkiragu05@gmail.com>
2024-03-01 04:24:59 -0800

    00

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-import urllib.request
+"""
+This script fetches https://alx-intranet.hbtn.io/status.
+It utilizes urlib package
+"""
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content.decode('utf-8'))
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        content = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
