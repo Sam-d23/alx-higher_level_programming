0aa5332199e407dd8f433208f9fb146af1bc00c2
Sam-d23 <samkiragu05@gmail.com>
2024-03-01 04:15:24 -0800

    0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content.decode('utf-8'))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+Project tasks for the 0x11. Python - Network #1 project
