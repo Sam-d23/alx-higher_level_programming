9587532f43e7817a4e80f65e1aa5a2aa0e8c3cb8
Sam-d23 <samkiragu05@gmail.com>
2024-03-27 11:12:12 -0700

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function readFile(filePath) {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('An error occurred while reading the file:', err);
-        } else {
-            console.log(data);
-        }
-    });
-}
-
-if (process.argv.length !== 3) {
-    console.error('Usage: node script.js <file_path>');
-    process.exit(1);
-}
-
 const filePath = process.argv[2];
-readFile(filePath);
+
+fs.readFile(filePath, 'utf8', function (error, content) {
+  if (error) {
+    console.error('An error occurred while reading the file:', error);
+  } else {
+    console.log(content);
+  }
+});
