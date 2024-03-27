b6abae504b46c0a760ac4e5c299c6419fc51b7a1
Sam-d23 <samkiragu05@gmail.com>
2024-03-27 11:20:06 -0700

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function writeFile(filePath, content) {
-    fs.writeFile(filePath, content, 'utf8', (err) => {
-        if (err) {
-            console.error('An error occurred while writing to the file:', err);
-        } else {
-            console.log('Content has been successfully written to the file.');
-        }
-    });
-}
-
-if (process.argv.length !== 4) {
-    console.error('Usage: node script.js <file_path> <content>');
-    process.exit(1);
-}
-
 const filePath = process.argv[2];
 const content = process.argv[3];
-writeFile(filePath, content);
+
+fs.writeFile(filePath, content, 'utf8', function (error) {
+  if (error) {
+    console.error('An error occurred while writing to the file:', error);
+  } else {
+    console.log('Content successfully written to the file.');
+  }
+});
