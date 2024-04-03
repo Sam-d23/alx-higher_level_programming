9669263237cf366479f23b1cfea23f740103c5e3
Sam-d23 <samkiragu05@gmail.com>
2024-04-03 12:35:56 -0700

    Task 2

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- Include jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+  <header>Header Text</header>
+  <script>
+    $('header').css('color', '#FF0000');
+  </script>
+</body>
+</html>
