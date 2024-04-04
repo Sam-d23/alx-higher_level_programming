245ac063a2f2e438a5b774b50a9bd28f711b2540
Sam-d23 <samkiragu05@gmail.com>
2024-04-03 17:30:31 -0700

    Task 0

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,15 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <header>Header Text</header>
-  <script>
-	const header = document.querySelector('header');
-	header.style.color = '#FF0000';
-  </script>
-</body>
-</html>
+const header = document.querySelector('header');
+header.style.color = '#FF000;
